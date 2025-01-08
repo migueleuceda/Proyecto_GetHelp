66cb43cec888c1ec76873a4b7514901c5166e992
migueleuceda <miguelssj.euceda7@gmail.com>
2025-01-08 20:15:42 +0100

    src

---
+++ src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getDatabase, provideDatabase } from '@angular/fire/database';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [
+    provideFirebaseApp(() => initializeApp({"projectId":"projecteangular-gethelp","appId":"1:974386551404:web:cab7b1e58f11863678d649","databaseURL":"https://projecteangular-gethelp-default-rtdb.firebaseio.com","storageBucket":"projecteangular-gethelp.firebasestorage.app","apiKey":"AIzaSyBhI14NyPE9XGPNU4z7xVPfZwf-2AWeVmQ","authDomain":"projecteangular-gethelp.firebaseapp.com","messagingSenderId":"974386551404","measurementId":"G-741PY21YRF"})),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
+    provideDatabase(() => getDatabase())
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+    production: true,
+    fireBaseConfig: {
+        apiKey: "AIzaSyBhI14NyPE9XGPNU4z7xVPfZwf-2AWeVmQ",
+        authDomain: "projecteangular-gethelp.firebaseapp.com",
+        projectId: "projecteangular-gethelp",
+        storageBucket: "projecteangular-gethelp.firebasestorage.app",
+        messagingSenderId: "974386551404",
+        appId: "1:974386551404:web:3d59c2b6b10366fe78d649",
+        measurementId: "G-Q0J0LPY4L2"
+    } 
+};
